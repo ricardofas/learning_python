3499e55043fc792d91120f00be1c5060e75cd04d
EC2 Default User <ec2-user@ip-172-31-38-157.eu-west-1.compute.internal>
2019-08-27 08:24:07 +0000

    added printing format examples

---
+++ printing_format.py
@@ -0,0 +1,15 @@
+#printing format stuff 
+
+print("This is a {} ".format("test"))
+
+#with variables 
+
+print("My name is {name} ".format(name="Rick metal"))
+
+#and with numbers 
+
+print("My name is {name} and im {years} years ".format(name="Rick metal",years="90"))
+
+#with floats
+
+print("My name is {name} and im {years} years and my salary is {salary:4.3f} dollars".format(name="Rick metal",years="90",salary=7034.034567))
